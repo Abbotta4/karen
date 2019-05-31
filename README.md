c2f3c83f362c4d083a5caeac6f2b4b72e3fd8550
Drew Abbott <abbotta4@gmail.com>
2019-05-31 00:50:44 -0400

    Reorganize list and edit select.py
    select.py now takes a single argument to choose if you want to randomly
    select either a TV show or a movie. The list has been reorganized to
    explicitly separate TV shows and movies.

--- README.md
+++ README.md
@@ -1,16 +1,12 @@
-# Movie/TV List
+# Movie List
 ###### (in no particular order)
 
 * Birdbox (2018)
 * ~~It Follows (2014)~~
-* Chilling Adventures of Sabrina (2018)
 * ~~Babel (2006)~~
 * ~~Rosemary's Baby (1968)~~
 * ~~The Ballad of Buster Scruggs (2018)~~
 * Death Proof (2007)
-* Dark (2017)
-* Penny Dreadful (2016)
-* Lore (2017)
 * ~~Silence of the Lambs (1991)~~
 * Cabin in the Woods (2012)
 * Shutter (2004)
@@ -20,7 +16,15 @@
 * Hush (2016)
 * ~~Don't Breathe (2016)~~
 * ~~A Quiet Place (2018)~~
-* Documentary Now! (2015)
 * Velvet Buzzsaw (2019)
 * What We Do in the Shadows (2014)
 * Corpse Bride (2005)
+
+# TV List
+###### (in no particular order)
+
+* Chilling Adventures of Sabrina (2018)
+* Dark (2017)
+* Penny Dreadful (2016)
+* Lore (2017)
+* Documentary Now! (2015)
--- select.py
+++ select.py
@@ -1,12 +1,29 @@
 from random import choice
+from sys import argv
+
+if len(argv) is not 2 or argv[1] not in ['movie', 'tv']:
+    print("Usage: python " + __file__ + " (movie | tv)")
+    exit(1)
 
 with open("README.md") as f:
     lines = f.readlines()
 
+mIndex = -1
+tIndex = -1
+for i, line in enumerate(lines):
+    if line.startswith("# Movie"):
+        mIndex = i + 3
+    if line.startswith("# TV"):
+        tIndex = i + 3
+
 choices = []
-    
-for line in lines:
-    if line.startswith("* ") and line.startswith("* ~~") == False:
-        choices.append(line[2:-1])
+if argv[1] == 'movie':
+    for movie in lines[mIndex:tIndex - 4]:
+        if movie.startswith("* ~~") is False:
+            choices.append(movie[2:-1])
+if argv[1] == 'tv':
+    for show in lines[tIndex:]:
+        if show.startswith("* ~~") is False:
+            choices.append(show[2:-1])
 
 print(choice(choices))        
